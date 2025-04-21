ec7f2ea39a41e21272327d7b3e8319a611016637
teoMarinov <teodor.b.marinov@gmail.com>
2025-04-21 19:49:11 +0300

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import AppRoutes from "./routes";
+import Header from "./component/Header";
 
 const App: React.FC = () => {
   return (
     <Router>
       <AuthProvider>
+        <Header />
         <AppRoutes />
       </AuthProvider>
     </Router>
