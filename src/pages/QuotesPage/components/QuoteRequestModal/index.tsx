87bf72be5a80b9b05c06bdc0c094cca3015bf1c7
teoMarinov <teodor.b.marinov@gmail.com>
2025-04-21 21:38:44 +0300

    swap fields and cleanup code

--- src/pages/QuotesPage/components/QuoteRequestModal/index.tsx
+++ src/pages/QuotesPage/components/QuoteRequestModal/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Modal, Box, Typography, Button, Divider } from "@mui/material";
 import { QuoteRequestFull } from "../../types";
+import { Modal, Box, Typography, Button, Divider } from "@mui/material";
 
 interface QuoteRequestModalProps {
   open: boolean;
@@ -19,49 +19,63 @@ const QuoteRequestModal: React.FC<QuoteRequestModalProps> = ({
     <Modal open={open} onClose={onClose}>
       <Box
         sx={{
-          position: "absolute",
+          p: 3,
           top: "50%",
           left: "50%",
-          transform: "translate(-50%, -50%)",
           width: 500,
-          maxWidth: '90dvw',
-          bgcolor: "background.paper",
           boxShadow: 24,
-          p: 3,
           borderRadius: 1,
-          border: `divider`
+          maxWidth: "90dvw",
+          border: `divider`,
+          position: "absolute",
+          bgcolor: "background.paper",
+          transform: "translate(-50%, -50%)",
         }}
       >
         <Typography
           variant="h6"
           gutterBottom
-          sx={{ fontWeight: "bold", color: "primary.main", textAlign: "center" }}
+          sx={{
+            fontWeight: "bold",
+            color: "primary.main",
+            textAlign: "center",
+          }}
         >
           Quote Request Details
         </Typography>
+
         <Typography variant="subtitle2" gutterBottom color="textSecondary">
           Created At: {new Date(data.createdAt).toLocaleString()}
         </Typography>
-        <Divider sx={{ mb: 1, borderColor: "divider" }} />{" "}
+
+        <Divider sx={{ mb: 1, borderColor: "divider" }} />
+
         <Typography variant="body2" sx={{ mb: 0.5 }}>
           <strong>Price Range:</strong> {data.priceRange}
         </Typography>
-        <Divider sx={{ my: 1, borderColor: "divider" }} />{" "}
-        <Typography variant="body2" sx={{ mb: 0.5 }}>
-          <strong>Service:</strong> {data.service}
-        </Typography>
+
+        <Divider sx={{ my: 1, borderColor: "divider" }} />
+
         <Typography variant="body2" sx={{ mb: 0.5 }}>
           <strong>Full Name:</strong> {data.fullname}
         </Typography>
+
+        <Typography variant="body2" sx={{ mb: 0.5 }}>
+          <strong>Service:</strong> {data.service}
+        </Typography>
+
         <Typography variant="body2" sx={{ mb: 0.5 }}>
           <strong>Email:</strong> {data.email}
         </Typography>
+
         <Typography variant="body2" sx={{ mb: 0.5 }}>
           <strong>Phone Number:</strong> {data.phoneNumber}
         </Typography>
+
         <Typography variant="body2" sx={{ mb: 0.5 }}>
           <strong>Company:</strong> {data.company}
         </Typography>
+
         <Box mt={2} textAlign="right">
           <Button
             variant="contained"
