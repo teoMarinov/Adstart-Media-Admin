727903ef6452528fb911a8b930df74d73a061eec
teoMarinov <teodor.b.marinov@gmail.com>
2025-04-21 21:30:18 +0300

    Add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import AppRoutes from "./routes";
 import Header from "./component/Header";
+import theme from "./theme";
+import { ThemeProvider } from "@emotion/react";
 
 const App: React.FC = () => {
   return (
     <Router>
       <AuthProvider>
-        <Header />
-        <AppRoutes />
+        <ThemeProvider theme={theme}>
+          <Header />
+          <AppRoutes />
+        </ThemeProvider>
       </AuthProvider>
     </Router>
   );
